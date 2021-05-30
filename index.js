cc7a795a83c125bc47ad5946c20fc0920e56de7c
Tom Horan <tomhoran91@gmail.com>
2021-05-30 18:39:23 +0200

    Map functioning

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-
-//$('h1').css('color', 'red')
-
-// Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
+    const carlingford = { lat: -25.344, lng: 131.036 };
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
+      zoom: 10,
+      center: carlingford,
     });
-    // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
-      position: uluru,
+      position: carlingford,
       map: map,
     });
   }
